3643bee49b1972f5a6f793c548c639ffb42f8ac6
Goonmaster <rodiongolovnin12@gmail.com>
2025-08-22 15:58:02 +0000

    FirebaseError: Failed to get document because the client is offline.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,14 +9,21 @@ import { getStorage } from 'firebase/storage';
 // You can get this from the Firebase console:
 // Project settings > General > Your apps > Web app > Firebase SDK snippet > Config
 const firebaseConfig = {
-  "projectId": "YOUR_PROJECT_ID",
-  "appId": "YOUR_APP_ID",
-  "storageBucket": "YOUR_STORAGE_BUCKET",
-  "apiKey": "YOUR_API_KEY",
-  "authDomain": "YOUR_AUTH_DOMAIN",
-  "measurementId": "YOUR_MEASUREMENT_ID",
-  "messagingSenderId": "YOUR_MESSAGING_SENDER_ID"
+
+  apiKey: "AIzaSyCJhbJ9Hx4ZzDneeSrPE-W1Hh7ifI1Ydxw",
+
+  authDomain: "coo-messenger-dut4g.firebaseapp.com",
+
+  projectId: "coo-messenger-dut4g",
+
+  storageBucket: "coo-messenger-dut4g.appspot.com",
+
+  messagingSenderId: "289105120218",
+
+  appId: "1:289105120218:web:0a828e96df9dc829edcee3"
+
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);

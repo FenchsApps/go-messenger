2643513e46dff16b0a32ef921b1e1575d7144446
Goonmaster <rodiongolovnin12@gmail.com>
2025-08-22 17:49:52 +0000

    Ладно, перейду к другой просьбе.
    Я сейчас опишу логотип нашего мессендж

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,13 +14,32 @@ export function PigeonIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      {/* Pigeon Body */}
       <path d="M10.2,3.2A2.47,2.47,0,0,1,12.6,3a2.38,2.38,0,0,1,2.3,1.9" />
-      <path d="M14.7,5.5a2.12,2.12,0,0,1-1.9-1.3" />
-      <path d="M22,9.3l-2,2.5a2.27,2.27,0,0,1-3.6-1L15,9.2a4.47,4.47,0,0,0-7.6-1.9" />
-      <path d="M13.4,13.2a2.5,2.5,0,0,1,3.4-3.4" />
       <path d="M9.2,12.7A2.35,2.35,0,0,0,4,13.6a2,2,0,0,0-1.2.5,2.2,2.2,0,0,0-.8,1.6,2.35,2.35,0,0,0,1.1,2.2,2.5,2.5,0,0,0,2.1.2l6.2-2.1" />
       <path d="M13,22a2,2,0,0,0,2-2" />
       <path d="M10,18a2,2,0,0,1-2,2" />
+      <path d="M14.7,5.5a2.12,2.12,0,0,1-1.9-1.3" />
+      
+      {/* Postcard with "Go" */}
+      <g transform="rotate(25 16 9)">
+        <rect x="13" y="7" width="10" height="7" rx="1" fill="hsl(var(--card))" stroke="currentColor" />
+        <text
+          x="18"
+          y="12.5"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize="5"
+          fontWeight="bold"
+          fill="currentColor"
+          stroke="none"
+        >
+          Go
+        </text>
+      </g>
+      
+      {/* Pigeon Head and Wing */}
+      <path d="M20,9.3l-2,2.5a2.27,2.27,0,0,1-3.6-1L13,9.2a4.47,4.47,0,0,0-6.6-1.9" />
     </svg>
   );
 }

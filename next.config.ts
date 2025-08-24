21366677596361caca572466ebf1049dc5e8a4bc
Goonmaster <rodiongolovnin12@gmail.com>
2025-08-24 15:00:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -70,6 +71,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.toptal.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };

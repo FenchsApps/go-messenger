ba499b53071bae64dee059c1fd10fad0c31f46ed
Goonmaster <rodiongolovnin12@gmail.com>
2025-08-24 15:10:33 +0000

    FirebaseError: Messaging: We are unable to register the default service

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJhbJ9Hx4ZzDneeSrPE-W1Hh7ifI1Ydxw",
+  authDomain: "coo-messenger-dut4g.firebaseapp.com",
+  projectId: "coo-messenger-dut4g",
+  storageBucket: "coo-messenger-dut4g.appspot.com",
+  messagingSenderId: "289105120218",
+  appId: "1:289105120218:web:0a828e96df9cee3"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

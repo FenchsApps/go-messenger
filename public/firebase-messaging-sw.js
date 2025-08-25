ce26d785c3cb1cba1041fb39d2de5abb0d94dd6e
Goonmaster <rodiongolovnin12@gmail.com>
2025-08-25 11:21:51 +0000

    А если я закрою браузер на телефоне?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,8 @@
-// @ts-nocheck
-importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCJhbJ9Hx4ZzDneeSrPE-W1Hh7ifI1Ydxw",
   authDomain: "coo-messenger-dut4g.firebaseapp.com",
@@ -13,49 +12,40 @@ const firebaseConfig = {
   appId: "1:289105120218:web:0a828e96df9cee3"
 };
 
-firebase.initializeApp(firebaseConfig);
-
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   
-  // Customize notification here
   const notificationTitle = payload.data.title;
   const notificationOptions = {
     body: payload.data.body,
     icon: payload.data.icon,
     data: {
-        url: payload.data.url // Pass the URL to the click event
+        url: payload.data.url
     }
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-
-    const urlToOpen = event.notification.data.url;
-
+    const urlToOpen = event.notification.data.url || '/';
     event.waitUntil(
         clients.matchAll({
-            type: 'window',
+            type: "window",
             includeUncontrolled: true
         }).then(function(clientList) {
-            // If a window is already open, focus it
             for (var i = 0; i < clientList.length; i++) {
                 var client = clientList[i];
-                // Check if the client URL matches. If so, focus it.
-                if (client.url === self.location.origin + '/' && 'focus' in client) {
-                    return client.focus().then(client => client.navigate(urlToOpen));
-                }
+                if (client.url == urlToOpen && 'focus' in client)
+                    return client.focus();
             }
-            // If no window is open, open a new one
-            if (clients.openWindow) {
+            if (clients.openWindow)
                 return clients.openWindow(urlToOpen);
-            }
         })
     );
 });
